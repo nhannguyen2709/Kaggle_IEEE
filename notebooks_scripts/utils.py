7b6078eccf791cb4e0e5b045eb7e0eb2c75f33a9
nhannguyen2709 <nhan.nguyen@eastagile.com>
2018-01-17 06:46:37 -0800

    Modify the train_on_multi_gpus to display weighted accuracy

--- notebooks_scripts/utils.py
+++ notebooks_scripts/utils.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-utility functions
+This script contains useful utility functions for fine-tuning the InceptionResNetV2 model.
+The `DirectoryIterator` class in `keras.preprocessing.image` module has been modified so that
+a tuple (inputs, targets, sample_weights) is returned instead of a tuple (inputs, targets).
 """
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD, RMSprop, Adam
 from keras.utils import multi_gpu_model
 
 from inception_resnet_v2 import InceptionResNetV2
-from resnet50 import ResNet50
-from resnet152 import ResNet152
 
 train_dir = "/mnt/safe01/data/train/"
 validation_dir = "/mnt/safe01/data/validation/"
@@ -39,21 +42,13 @@ def train_validation_generator(batch_size, img_height_width):
         class_mode='categorical')
     return train_generator, validation_generator
 
-# load pre-trained weights onto CPU
 def load_pretrained_weights(network_name, input_shape):
     if network_name == 'InceptionResNetV2':
         with tf.device('/cpu:0'):
             model = InceptionResNetV2(include_top=False, pooling='avg_max',
                                       input_shape=input_shape)
-    elif network_name == 'ResNet50':
-        with tf.device('/cpu:0'):
-            model = ResNet50(include_top=False, input_shape=input_shape)
-    elif network_name == 'ResNet152':
-        with tf.device('/cpu:0'):
-            model = ResNet152(img_rows=512, img_cols=512, num_channels=3, num_classes=10)
     return model
 
-# freeze layers
 def freeze_layers(list_of_names, model, trainable_layers_names, freeze_proportion=0):
     """
     If list_of_names is True then we can specify which layers to be freezed by trainable_layers_names. 
@@ -72,42 +67,32 @@ def freeze_layers(list_of_names, model, trainable_layers_names, freeze_proportio
             layer.trainable = False
         for layer in model.layers[int(num_layers*freeze_proportion):]:
             layer.trainable = True
-            
-def train_on_multi_gpus(parallel_model, train_generator, 
-                        validation_generator, num_train_samples, num_valid_samples,
-                        batch_size, optimizer, learning_rate, epochs):
+
+def train_on_multi_gpus(parallel_model, train_generator, validation_generator, 
+                        num_train_samples, num_valid_samples, batch_size, 
+                        optimizer, learning_rate, epochs):
     """
-    Wrap the fit_generator() function of a parallel model inside and return the History object.
+    Compile the parallel model with different optimizers and hyperparameters settings,
+    tracking accuracy (non-weighted) metric on training and validation sets.
+    Wrap the fit_generator() function of the parallel model inside and return the History object.
     """
     if optimizer == 'sgd':
         parallel_model.compile(loss='categorical_crossentropy',
                                optimizer=SGD(lr=learning_rate,
                                              decay=learning_rate / epochs),
-                               metrics=['accuracy'])
-        history = parallel_model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train_samples // batch_size,
-            epochs=epochs,
-            validation_data=validation_generator,
-            validation_steps=num_valid_samples // batch_size)
+                               weighted_metrics=['accuracy'])
     elif optimizer == 'adam':
         parallel_model.compile(loss='categorical_crossentropy',
                                optimizer=Adam(lr=learning_rate),
-                               metrics=['accuracy'])
-        history = parallel_model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train_samples // batch_size,
-            epochs=epochs,
-            validation_data=validation_generator,
-            validation_steps=num_valid_samples // batch_size)
+                               weighted_metrics=['accuracy'])
     elif optimizer == 'rmsprop':
         parallel_model.compile(loss='categorical_crossentropy',
                                optimizer=RMSprop(lr=learning_rate, decay=0.9),
-                               metrics=['accuracy'])
-        history = parallel_model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train_samples // batch_size,
-            epochs=epochs,
-            validation_data=validation_generator,
-            validation_steps=num_valid_samples // batch_size)
+                               weighted_metrics=['accuracy'])
+    history = parallel_model.fit_generator(
+        train_generator,
+        steps_per_epoch=num_train_samples // batch_size + 1,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=num_valid_samples // batch_size + 1)
     return history

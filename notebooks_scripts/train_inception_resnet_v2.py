7df04997edf05925fd4e58973b62bc28cb7db8e7
nhannguyen2709 <nhan.nguyen@eastagile.com>
2018-01-21 20:00:11 -0800

    Split training process into smaller chunks

--- notebooks_scripts/train_inception_resnet_v2.py
+++ notebooks_scripts/train_inception_resnet_v2.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This script is used to fine-tune the InceptionResNetV2. Training process can be split into two stages.
-First stage includes training the bottom 10% layers for 200 epochs using Adam optimizer 
-with decayed learning_rate initialized at 1e-4.
-Second stage includes training only the 3 last Dense layers for 100 epochs using SGD optimizer 
-with decayed learning_rate initialized at 5e-3.
+This script is used to fine-tune the InceptionResNetV2. Training process can be split into multiple stages.
 """
 import tensorflow as tf
 import keras.backend as K
@@ -20,27 +16,29 @@ K.set_session(session)
 
 weights_path = "/home/nhannguyen/Kaggle_IEEE/model_weights/inception_resnet_v2/"
 
-train_generator, validation_generator = train_validation_generator(batch_size=36,
-                                                                   img_height_width=(512, 512))
+train_generator, validation_generator = train_validation_generator(batch_size=50,
+                                                                   img_height_width=(299, 299))
  
 # create computational graph and load pre-trained ImageNet weights
-inceptionresnetv2 = load_pretrained_weights('InceptionResNetV2', input_shape=(512, 512, 3))
+inceptionresnetv2 = load_pretrained_weights('InceptionResNetV2', input_shape=(299, 299, 3))
 
 # overwrite ImageNet weights with weights obtained from last training epoch
 inceptionresnetv2.load_weights(weights_path + 'multigpu.hdf5')
 
 # fine-tune
-parallel_model = multi_gpu_model(model=inceptionresnetv2, gpus=4)
+parallel_model = multi_gpu_model(model=inceptionresnetv2, gpus=3)
 freeze_layers(list_of_names=False,
               trainable_layers_names=None,
               model=inceptionresnetv2,
-              freeze_proportion=0.6)
-history = train_on_multi_gpus(parallel_model=parallel_model,
-                              train_generator=train_generator,
-                              validation_generator=validation_generator,
-                              num_train_samples=22319, num_valid_samples=2430,
-                              batch_size=36, optimizer='sgd', 
-                              learning_rate=5e-2, epochs=30)
-# save weights after finishing training
-inceptionresnetv2.save_weights(weights_path + 'multigpu.hdf5')
-print("Successfully saved weights onto disk")
+              freeze_proportion=0.9)
+multi_stages_epochs = [5, 5, 10, 10, 10, 20, 20, 20]
+for i, stage_epochs in enumerate(multi_stages_epochs):
+    history = train_on_multi_gpus(parallel_model=parallel_model,
+                                  train_generator=train_generator,
+                                  validation_generator=validation_generator,
+                                  num_train_samples=490050, num_valid_samples=4950,
+                                  batch_size=50, optimizer='adam', 
+                                  learning_rate=1e-3, epochs=stage_epochs)
+    save_path = weights_path + 'multigpu' + str(i) + '.hdf5'
+    inceptionresnetv2.save_weights(save_path)
+    print("Successfully saved weights obtain from training stage {} onto disk".format(i))

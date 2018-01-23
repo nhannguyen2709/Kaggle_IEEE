1e052cfe68189f82c7bd837cfd4df712c5d07035
nhannguyen2709 <nhan.nguyen@eastagile.com>
2018-01-22 23:32:02 -0800

    Update learning rate for training stages

--- notebooks_scripts/train_inception_resnet_v2.py
+++ notebooks_scripts/train_inception_resnet_v2.py
@@ -23,22 +23,24 @@ train_generator, validation_generator = train_validation_generator(batch_size=50
 inceptionresnetv2 = load_pretrained_weights('InceptionResNetV2', input_shape=(299, 299, 3))
 
 # overwrite ImageNet weights with weights obtained from last training epoch
-inceptionresnetv2.load_weights(weights_path + 'multigpu.hdf5')
+inceptionresnetv2.load_weights(weights_path + 'multigpu0.hdf5')
 
 # fine-tune
-parallel_model = multi_gpu_model(model=inceptionresnetv2, gpus=3)
 freeze_layers(list_of_names=False,
               trainable_layers_names=None,
               model=inceptionresnetv2,
               freeze_proportion=0.9)
+parallel_model = multi_gpu_model(model=inceptionresnetv2, gpus=4)
 multi_stages_epochs = [5, 5, 10, 10, 10, 20, 20, 20]
+multi_stages_learning_rate = [1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-4, 1e-4, 1e-5]
 for i, stage_epochs in enumerate(multi_stages_epochs):
     history = train_on_multi_gpus(parallel_model=parallel_model,
                                   train_generator=train_generator,
                                   validation_generator=validation_generator,
                                   num_train_samples=490050, num_valid_samples=4950,
                                   batch_size=50, optimizer='adam', 
-                                  learning_rate=1e-3, epochs=stage_epochs)
+                                  learning_rate=multi_stages_learning_rate[i], 
+                                  epochs=stage_epochs)
     save_path = weights_path + 'multigpu' + str(i) + '.hdf5'
     inceptionresnetv2.save_weights(save_path)
     print("Successfully saved weights obtain from training stage {} onto disk".format(i))

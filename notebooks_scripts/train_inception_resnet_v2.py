714b9812b0ddcb332d5895995fcac5b2007274b9
nhannguyen2709 <nhan.nguyen@eastagile.com>
2018-03-01 10:27:55 -0800

    provide arguments and hyperparameters to be set

--- notebooks_scripts/train_inception_resnet_v2.py
+++ notebooks_scripts/train_inception_resnet_v2.py
@@ -14,31 +14,43 @@ config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 K.set_session(session)
 
-weights_path = "/home/nhannguyen/Kaggle_IEEE/model_weights/inception_resnet_v2/"
 
-train_generator, validation_generator = train_validation_generator(batch_size=50,
-                                                                   img_height_width=(299, 299))
+# arguments to set
+NUM_GPUS = 4
+IMG_HEIGHT_WIDTH = (299, 299)
+INPUT_SHAPE = (299, 299, 3)
+BATCH_SIZE = 64
+OPTIMIZER = 'adam'
+FREEZE_PROPORTION = 0.99
+LIST_OF_NAMES = False
+
+# data generators 
+weights_path = "/home/nhannguyen/Kaggle_IEEE/model_weights/inception_resnet_v2/"
+train_generator, validation_generator = train_validation_generator(batch_size=BATCH_SIZE,
+                                                                   img_height_width=IMG_HEIGHT_WIDTH)
+NUM_TRAIN_SAMPLES = 147060
+NUM_VALID_SAMPLES = 1440
  
 # create computational graph and load pre-trained ImageNet weights
-inceptionresnetv2 = load_pretrained_weights('InceptionResNetV2', input_shape=(299, 299, 3))
+inceptionresnetv2 = load_pretrained_weights('InceptionResNetV2', input_shape=INPUT_SHAPE)
 
 # overwrite ImageNet weights with weights obtained from last training epoch
 inceptionresnetv2.load_weights(weights_path + 'multigpu7.hdf5')
 
 # fine-tune
-freeze_layers(list_of_names=False,
+freeze_layers(list_of_names=LIST_OF_NAMES,
               trainable_layers_names=None,
               model=inceptionresnetv2,
-              freeze_proportion=0.9)
-parallel_model = multi_gpu_model(model=inceptionresnetv2, gpus=4)
+              freeze_proportion=FREEZE_PROPORTION)
+parallel_model = multi_gpu_model(model=inceptionresnetv2, gpus=NUM_GPUS)
 multi_stages_epochs = [10, 10, 10, 20, 20, 20, 20, 20]
 multi_stages_learning_rate = [1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-4, 1e-4, 1e-4]
 for i, stage_epochs in enumerate(multi_stages_epochs):
     history = train_on_multi_gpus(parallel_model=parallel_model,
                                   train_generator=train_generator,
                                   validation_generator=validation_generator,
-                                  num_train_samples=490050, num_valid_samples=4950,
-                                  batch_size=50, optimizer='adam', 
+                                  num_train_samples=NUM_TRAIN_SAMPLES, num_valid_samples=NUM_VALID_SAMPLES,
+                                  batch_size=BATCH_SIZE, optimizer=OPTIMIZER, 
                                   learning_rate=multi_stages_learning_rate[i], 
                                   epochs=stage_epochs)
     save_path = weights_path + 'multigpu' + str(i) + '.hdf5'
